17307303b7285128a2224d9865eeac184d31c6cd
Richard Huang <rickypc@users.noreply.github.com>
2018-04-07 11:57:12 -0700

    Adjust the whitespaces.

--- gulpfile.js
+++ gulpfile.js
@@ -240,6 +240,6 @@ gulp.task('common:test', function(cb) {
 //gulp.task('watch', function() {
 //});
 
-gulp.task('default', [ 'clean' , 'chrome'], function(cb) {
+gulp.task('default', [ 'clean', 'chrome' ], function(cb) {
     cb();
 });
